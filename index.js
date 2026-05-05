d8b37a1c41d076d57be43ed071f59e7703c3a5e6
Louis <yt.gaming.pixel@gmail.com>
2026-05-05 22:22:23 +0100

    fix: global CORS middleware

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+});
 
 const {
   Client,
